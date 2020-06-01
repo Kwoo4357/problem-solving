0cc2effec0972301300ca3f322bed3f9f5db40a6
Kwoo <dlrjsdn4357@gmail.com>
2020-06-01 09:52:32 +0900

    BOJ 1028 다이아몬드 광산 solution. dp 문제라는데... 이 풀이가 dp라고 할 수는 없을 듯. 시간 소모가 적은 코드를 보고 공부 필요

---
+++ boj/dynamic_programming/1028.py
@@ -0,0 +1,20 @@
+#다이아몬드 광산
+import sys
+input = sys.stdin.readline
+
+r, c = map(int, input().split())
+mine = [[int(c) for c in input().rstrip()] for _ in range(r)]
+rd = [mine[i].copy() for i in range(r)]
+ld = [mine[i].copy() for i in range(r)]
+ans = max(mine[0])
+
+for i in range(1, r):
+    for j in range(c):
+        if mine[i][j]:
+            rd[i][j] = rd[i-1][j-1] + 1 if j > 0 else rd[i][j]
+            ld[i][j] = ld[i-1][j+1] + 1 if j < c-1 else ld[i][j]
+            t = min(rd[i][j], ld[i][j])
+            temp = max([a+1 for a in range(t) if rd[i-a][j+a] > a and ld[i-a][j-a] > a])
+            ans = max(ans, temp)
+
+print(ans)
