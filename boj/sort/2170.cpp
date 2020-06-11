fa66557e4c05bb07bd2191f0bcd629b2e4a20917
Kwoo <dlrjsdn4357@gmail.com>
2020-06-11 16:55:06 +0900

    BOJ 2170 선 긋기 solution. 아이디어를 떠올린다면 정렬이 성능에 더 영향이 큰 문제

---
+++ boj/sort/2170.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n, i, start, end, ans=0;
+    pair<int, int> lines[1000000];
+
+    scanf("%d", &n);
+    for(i=0; i<n; i++) scanf("%d%d", &lines[i].first, &lines[i].second);
+    sort(lines, lines+n);
+
+    i = -1;
+    while(++i<n){
+        start = lines[i].first;
+        end = lines[i].second;
+
+        while(i<n-1 && end > lines[i+1].first) end = max(end, lines[++i].second);
+        ans += end-start;
+    }
+
+    printf("%d", ans);
+}
